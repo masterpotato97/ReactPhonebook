34ac76807ae3e6b352d9ed30a35e07a1f630e7a9
Daniel Herrera <dannyherrea97@gmail.com>
2023-11-02 12:23:24 -0500

    First

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDsMWxZpjarRrM4LjWnjXIEliZoYMN3M4o",
+  authDomain: "phonebook-5f725.firebaseapp.com",
+  projectId: "phonebook-5f725",
+  storageBucket: "phonebook-5f725.appspot.com",
+  messagingSenderId: "323345451286",
+  appId: "1:323345451286:web:cae7645aee27b20f05a837"
+};
+
